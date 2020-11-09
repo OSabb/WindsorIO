df4064564df96200655325f19a0a9bdb0c8d4807
DeltaZulu <bbgun567@gmail.com>
2020-11-09 11:38:22 -0500

    Add files via upload

---
+++ html/login.php
@@ -0,0 +1,10 @@
+<?php
+    $hn = 'localhost';      //hostname
+    $db = 'zhou16g_soft';   //database
+    $un = 'zhou16g_soft';   //username
+    $pw = 'dojm';      //password
+    
+    //averageuser - "user"
+    //developer - "crying"
+    //administrator - "admin"
+?>
